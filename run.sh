bbddcdb6405bcf1c1c6780f1242b6e5e96363b5f
Tatiana Aksamit <tatiana.aksamit@globant.com>
2022-06-06 17:11:49 +0300

    run.sh was deleted

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Starting gathering bycard data"
-python3.9 send_to_kafka.py --src bycard &
-sleep 3  &
-echo "Starting gathering ticketpro data"
-python3.9 send_to_kafka.py --src ticketpro
